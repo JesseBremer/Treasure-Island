8a61cb184f2362b5efcc2f9442ffeffbcc7edcd4
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-12 11:40:02 -0400

    Logical Operators.

--- Logical Operators/task.py
+++ Logical Operators/task.py
@@ -2,6 +2,7 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 bill = 0
 
+
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
@@ -11,6 +12,9 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("Midlife crisis special: $0.")
     else:
         bill = 12
         print("Adult tickets are $12.")
