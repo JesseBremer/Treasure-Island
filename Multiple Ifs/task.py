ef48940ec860f47e27a803f91238d6752a2b9ed7
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-12 11:13:57 -0400

    multiple if statements and using global variables.

--- Multiple Ifs/task.py
+++ Multiple Ifs/task.py
@@ -1,31 +1,25 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        print("Children: $5.")
+        bill = 5
     elif age <= 18:
-        print("Please pay $7.")
+        print("Youth: $7.")
+        bill = 7
     else:
-        print("Please pay $12.")
+        print("Adult: $12.")
+        bill = 12
+
+    wants_photo = input("Do you want a photo taken? Type y for 'Yes' and n for 'No'.")
+    if wants_photo == "y":
+        bill += 3
+
+    print(f"Your total is: ${bill}.")
 else:
     print("Sorry you have to grow taller before you can ride.")
 
-weight = 85
-height = 1.85
-
-bmi = weight / (height ** 2)
-
-# 🚨 Do not modify the values above
-# Write your code below 👇
-
-if bmi < 18.5:
-    print("underweight")
-elif bmi < 25:
-    print("normal weight")
-elif bmi >= 25:
-    print("overweight")
-else:
-    print("Else Statement")
